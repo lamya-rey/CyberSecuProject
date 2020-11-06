85cfedab24400306f522223579c2712b7719c1d4
lamya-rey <61109480+lamya-rey@users.noreply.github.com>
2020-11-06 21:31:58 +0100

    1commit

---
+++ VenteVoiture/src/test/java/com/tp/spring/VenteVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteVoitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
