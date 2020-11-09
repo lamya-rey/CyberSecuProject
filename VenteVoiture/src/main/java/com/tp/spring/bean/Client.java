2dad6eea0f47acc67bb329e6cc00776666ae33fd
lamya-rey <61109480+lamya-rey@users.noreply.github.com>
2020-11-09 09:46:59 +0100

    client

--- VenteVoiture/src/main/java/com/tp/spring/bean/Client.java
+++ VenteVoiture/src/main/java/com/tp/spring/bean/Client.java
@@ -1,16 +1,9 @@
 package com.tp.spring.bean;
 
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 public class Client {
